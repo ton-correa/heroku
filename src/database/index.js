460a961ce329184d2d3fbe2a76916e1c0547131d
ton-correa <65102886+ton-correa@users.noreply.github.com>
2020-07-11 12:28:04 -0300

    banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
 const knexfile = require('../../knexfile');
-const knex = require('knex')(knexfile.development);
+const knex = require('knex')(knexfile.production || knexfile.development);
 
 module.exports = knex;
