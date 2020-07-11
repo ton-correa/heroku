316f3ba86f22f17394203f01eba5ff62b6994e08
ton-correa <65102886+ton-correa@users.noreply.github.com>
2020-07-11 15:16:13 -0300

    Teste 2

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
-const knexfile = require('../../knexfile');
+  
+const environment = process.env.NODE_ENV || 'development';
+const knexfile = require('../../knexfile')[environment];
 const knex = require('knex')(knexfile);
 
 module.exports = knex;
